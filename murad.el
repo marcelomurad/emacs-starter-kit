c562b40dc8ef4863970db79d8c209878602d8796
Marcelo Murad <marcelo.murad@dtmconsultoria.com>
2010-11-12 18:00:28 -0200

    Color-theme support.

--- murad.el
+++ murad.el
@@ -1,3 +1,7 @@
 (add-to-list 'load-path (concat dotfiles-dir "/reps/rvm.el"))
 (require 'rvm)
 (rvm-use-default) ;; use rvm's default ruby for the current Emacs session
+
+(add-to-list 'load-path (concat dotfiles-dir "/reps/color-theme-6.6.0/color-theme.el"))
+(require 'color-theme)
+(color-theme-blackboard)
