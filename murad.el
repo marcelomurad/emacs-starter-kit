4fcc8d99951b95163b2594113d02bd1cd3ef1b3d
Marcelo Murad <marcelo.murad@dtmconsultoria.com>
2010-11-13 11:51:42 -0200

    Rename directory reps to vendor.

--- murad.el
+++ murad.el
@@ -1,12 +1,12 @@
-(add-to-list 'load-path (concat dotfiles-dir "/reps/rvm.el"))
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/rvm.el"))
 (require 'rvm)
 (rvm-use-default) ;; use rvm's default ruby for the current Emacs session
 
-(add-to-list 'load-path (concat dotfiles-dir "/reps/color-theme-6.6.0/color-theme.el"))
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/color-theme-6.6.0/color-theme.el"))
 (require 'color-theme)
 (color-theme-blackboard)
 
-(add-to-list 'load-path (concat dotfiles-dir "/reps/yasnippet-0.6.1c"))
+(add-to-list 'load-path (concat dotfiles-dir "/vendor/yasnippet-0.6.1c"))
 (require 'yasnippet)
 (yas/initialize)
-(yas/load-directory "~/.emacs.d/reps/yasnippet-0.6.1c/snippets")
+(yas/load-directory "~/.emacs.d/vendor/yasnippet-0.6.1c/snippets")
