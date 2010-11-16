a4cb8ffecf66b0dae29d8337c04f3f471a48dc8e
Marcelo Murad <marcelo.murad@dtmconsultoria.com>
2010-11-16 15:40:03 -0200

    PHP mode support.

--- murad.el
+++ murad.el
@@ -14,3 +14,7 @@
 (yas/load-directory "~/.emacs.d/vendor/yasnippet-0.6.1c/snippets")
 
 (require 'textmate-to-yas)
+
+(autoload 'php-mode "php-mode" "Major mode for editing php code." t)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
